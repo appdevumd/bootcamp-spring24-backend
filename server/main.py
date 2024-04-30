415bba3fa787aa15f908e068a064dba37f959828
Aidan Melvin <aidmelvin.314@gmail.com>
2024-04-29 21:28:15 -0400

    final workshop code

--- server/main.py
+++ server/main.py
@@ -1,3 +1,23 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = ""
+
+# Create a new client and connect to the server
+client = MongoClient(uri, tlsAllowInvalidCertificates=True)
+
+print('testing connection')
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+print('Ran connection check')
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,6 +25,9 @@ app = FastAPI()
 from pydantic import BaseModel  # for creating API request models
 
 from fastapi.middleware.cors import CORSMiddleware  # for allowing cross-origin requests
+
+db = client.bootcampApp
+# db = client['bootcampApp']
 
 origins = [
     "*",
@@ -18,10 +41,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ['buy food', 'do hw']
-
-todo_list_items = []  # things will get stored here for now
-
 # class Item extends/implements BaseModel
 class Item(BaseModel):
     name: str
@@ -31,13 +50,20 @@ class Element(BaseModel):
 
 @app.get('/items')
 async def get_items():
-    return todo_list_items
+    collection = db.todolist
+    return list(collection.find())
+    # return todo_list_items
 
 @app.post('/add')
 async def add_item(item: Item):
-    todo_list_items.append(item.name)
+    collection = db.todolist
+    collection.insert_one(dict(item))
     return {'message': 'success'}
 
 @app.delete('/remove')
 async def remove_item(element: Element):
-    todo_list_items.pop(element.number)
+    collection = db.todolist
+    nth_item = collection.find().skip(element.number).limit(1)
+    collection.delete_one({'_id': nth_item[0]['_id']})
+
+    return {'message': 'success'}
